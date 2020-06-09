98b728b9be703703440ea5e5b7d8464a3c869736
andremlsantos <andremlsantos@gmail.com>
2020-06-09 18:46:04 +0200

    Connection to database
    Mongoose connect via async await

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
+// Connect to database
+connectDB();
 
 app.get("/", (req, res) => {
     res.status(200).json({
