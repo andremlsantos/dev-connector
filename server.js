fdba318712a92abd7cae16da36782cbb97550acd
andremlsantos <andremlsantos@gmail.com>
2020-06-09 19:01:13 +0200

    Route Files With Express Router
    Following tests routes were added:
    /api/auth/
    /api/users/
    /api/profiles/
    /api/posts/

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
-
 // Connect to database
 connectDB();
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "API RUNNING",
-    });
-});
+// Define routes
+app.use("/api/users/", require("./routes/api/users"));
+app.use("/api/posts/", require("./routes/api/posts"));
+app.use("/api/auth/", require("./routes/api/auth"));
+app.use("/api/profile/", require("./routes/api/profile"));
+
+// Start listening
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
