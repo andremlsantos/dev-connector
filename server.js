b9d7ca86e27bffb0f098ff52e2a8211af644a386
andremlsantos <andremlsantos@gmail.com>
2020-06-09 21:33:14 +0200

    User API Routes & JWT Authentication
    Creating The User Model

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const connectDB = require("./config/db");
 connectDB();
 
 // Define routes
-app.use("/api/users/", require("./routes/api/users"));
-app.use("/api/posts/", require("./routes/api/posts"));
+app.use("/api/users/", require("./routes/api/user"));
+app.use("/api/posts/", require("./routes/api/post"));
 app.use("/api/auth/", require("./routes/api/auth"));
 app.use("/api/profile/", require("./routes/api/profile"));
 
