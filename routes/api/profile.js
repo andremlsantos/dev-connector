35a88a774e74c1d0a514c348f3f4fa066ccddf64
andremlsantos <andremlsantos@gmail.com>
2020-06-12 22:48:01 +0200

    4. Profile API Routes
    Get Github Repos For Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator/check");
+const request = require("request");
+const config = require("config");
+const axios = require('axios');
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -253,6 +256,27 @@ router.delete("/education/:edu_id", [auth], async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).send("Server error");
+    }
+});
+
+// @route GET api/profile/github/:username
+// @desc Get user repositories from GitHub
+// @access Public
+router.get("/github/:username", async (req, res) => {
+    try {
+        const uri = encodeURI(
+            `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+        );
+        const headers = {
+            'user-agent': 'node.js',
+            Authorization: `token ${config.get('githubToken')}`
+        };
+
+        const gitHubResponse = await axios.get(uri, { headers });
+        return res.json(gitHubResponse.data);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(404).json({ msg: 'No Github profile found' });
     }
 });
 
